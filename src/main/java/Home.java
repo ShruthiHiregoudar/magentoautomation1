07a16e389ce58fb551f84fa6ffa1c9297c64a469
Shruthi Hiregoudar <shruthi.hiregoudar@gmail.com>
2018-07-01 11:11:11 +0530

    first commit

---
+++ src/main/java/Home.java
@@ -0,0 +1,20 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Home 
+{
+	WebDriver driver=null;
+	By myacct=By.xpath("//*[text()='Account']/ancestor::a");
+	public Home(WebDriver driver)
+	{
+		this.driver=driver;
+		System.out.println(driver);
+	}
+	public void clickOnMyacct()
+	{
+		driver.findElement(myacct).click();
+	}
+
+}
---
+++ src/main/java/Login.java
@@ -0,0 +1,28 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login 
+{
+	WebDriver driver=null;
+	By email=By.id("email");
+	By pwd=By.id("pass");
+	By login=By.id("send2");
+	public Login(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	public void sendEmail(String un)
+	{
+		driver.findElement(email).sendKeys(un);
+	}
+	public void sendPassword(String pwd1)
+	{
+		driver.findElement(pwd).sendKeys(pwd1);
+	}
+	public void clickLogin()
+	{
+		driver.findElement(login).click();
+	}
+}
