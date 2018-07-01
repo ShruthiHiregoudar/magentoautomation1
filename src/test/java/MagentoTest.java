bbde9008e6cbe15ea5e3000913b3b593b7b82718
Shruthi Hiregoudar <shruthi.hiregoudar@gmail.com>
2018-07-01 11:23:12 +0530

    second commit

--- src/test/java/MagentoTest.java
+++ src/test/java/MagentoTest.java
@@ -3,6 +3,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest 
@@ -10,7 +11,7 @@ public class MagentoTest
 	@Test
 	public void testrun()
 	{
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		String url="https://www.magento.com";
